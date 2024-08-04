aebbb65750f57c301547a74fd973aa6343ff63b2
kaliiiiiiiiii <aurinliun@gmx.ch>
2024-08-04 22:37:42 +0200

    improve styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ export default function Home() {
         <title>CAT ROOM</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </Head>
-      <div>
-        <a href="https://github.com/kaliiiiiiiiii/vercel_utils_server">Repo</a>
+      <div className="center">
+        <h1 className="title">A server used for various projects</h1>
+        <a href="https://github.com/kaliiiiiiiiii/vercel_utils_server" className="link">Repo</a>
       </div>
     </>
   );
