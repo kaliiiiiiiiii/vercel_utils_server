d95fe0aad45f6951744db1eb06c287f74bd6211d
kaliiiiiiiiiii <89038706+kaliiiiiiiiii@users.noreply.github.com>
2024-08-23 17:04:06 +0200

    Update route.ts

--- app/exehid/route.ts
+++ app/exehid/route.ts
@@ -9,12 +9,12 @@ export async function GET(req: NextRequest) {
     return new NextResponse('Bad Request: "url" query parameter is required\nsee https://github.com/kaliiiiiiiiii/vercel_utils_server for usage', { status: 400 });
   }
 
-  // Construct the new URL for the exe2ps1 endpoint
+  // parse to exe2ps1 url
   const encodedUrl = encodeURIComponent(url);
   const encodedArgs = args.map(arg => `arg=${encodeURIComponent(arg)}`).join('&');
   const exe2ps1Url = `${origin}/exe2ps1?url=${encodedUrl}${encodedArgs ? `&${encodedArgs}` : ''}`;
 
-  // Construct the redirect URL for the pshid endpoint
+  // parse redirect URL => execute exe2ps1 over pshid
   const redirectUrl = `${origin}/pshid?url=${encodeURIComponent(exe2ps1Url)}`;
 
   return NextResponse.redirect(redirectUrl, 302);
